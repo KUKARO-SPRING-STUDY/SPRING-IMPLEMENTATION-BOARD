40aaddf43cc908497d7acd545a466dbc452a8c0d
kukaro <justkukaro@naver.com>
2024-03-15 18:25:49 +0900

    feat: eager로 했는데 왜생기지??

--- src/main/java/org/example/springimplementationboard/Board/BoardRepository.java
+++ src/main/java/org/example/springimplementationboard/Board/BoardRepository.java
@@ -3,7 +3,9 @@ package org.example.springimplementationboard.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    @Query("SELECT b FROM BoardEntity b LEFT JOIN FETCH b.comments")
     Page<BoardEntity> findAll(Pageable pageable);
 }
